6ad7f95dfcd8d2071207e66498e76a4255db43af
vishwamohini15 <vishwamohini0015@gmail.com>
2024-09-26 20:40:39 +0530

    complete ch 6

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express=require('express');
 const morgan=require('morgan')
 const server= express();
 const productRouter=require('./routs/route-product');
 const userRouter=require('./routs/user');
+console.log("env",process.env.DB_PASSWORD);
+
 
 const { type } = require('os');
 const { Agent } = require('http');
@@ -10,15 +13,17 @@ const { Agent } = require('http');
 //body parser
 server.use(express.json())
 server.use(morgan('default'))
-server.use(express.static('public'))
+server.use(express.static(process.env.PUBLIC_DIR))
 server.use('/products', productRouter.router)
 server.use('/users', userRouter.router)
+
+
 
 
 //MVC modal-view-controller
 
 
-server.listen(8080, ()=>{
+server.listen(process.env.PORT, ()=>{
      console.log("server started");
      
 })

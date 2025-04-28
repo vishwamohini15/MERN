cd0d4004ffaac93b0b4f95a3eeae1969be0b2c6a
vishwamohini15 <vishwamohini0015@gmail.com>
2025-04-28 21:50:44 +0530

    ch2 webServer Using node

--- FullmernSikhnaHai/index.js
+++ FullmernSikhnaHai/index.js
@@ -1,39 +1,55 @@
 const http=require('http');
 // const { json } = require('stream/consumers');
 
-const fs=require('fs')
+const fs=require('fs');
+const { json } = require('stream/consumers');
 
 const index=fs.readFileSync('index.html', 'utf-8')
-const data=fs.readFileSync('data.json', 'utf-8')
+const data=JSON.parse(fs.readFileSync('data.json', 'utf-8'))
+
+const products=data.products;
+
 
 
 // const data={age:5};
 const server=http.createServer((req,res)=>{
+     console.log(req.url, req.method);
+     if (req.url.startsWith('/product')) {
+          const id=req.url.split('/')[2];          
+         const product= products.find(p=>p.id===(+id))
+         console.log(product);
+         res.setHeader('Content-Type', 'text/html')
+         let modyfyindex=index.replace('**title**', product.title).replace('**thumbnail**', product.thumbnail).replace('**price**', product.price).replace('**rating**', product.rating)
+               res.end(modyfyindex);
+               return;
+     }
 
-     console.log(req.url);
+              
 
      switch(req.url){
           case '/':
      res.setHeader('Content-Type', 'text/html')
           res.end(index);
           break;
+
           case '/api':
      res.setHeader('Content-Type', 'application/json')
-          res.end(data);
-          break
+          res.end(JSON.stringify(data));
+          break;
+          
           default:
-               res.end("hello")
-               break;
+               res.writeHead(404)
+               res.end();
      }
      
-     console.log('server started');
-     res.setHeader('dummy', 'dummyvalue')
+     console.log('server starte');
+     // res.setHeader('dummy', 'dummyvalue')
      // res.setHeader('Content-Type', 'application/json')
 
 
      // res.end("<h1>hello</h1>")
      // res.end(data)
-     res.end(index)
+     // res.end(index)
 
      // res.end(JSON.stringify(data));
 })
